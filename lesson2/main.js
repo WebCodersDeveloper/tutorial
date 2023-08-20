ad84e5224dd54cb988aa43021abe402947719015
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2023-08-20 10:35:24 +0500

    first commit

---
+++ lesson2/main.js
@@ -0,0 +1,324 @@
+// var name ='ali' 
+// let name = 'ali'
+// const name ='horror'
+
+// let x = 4;
+// let y = 16;
+
+// function add() {
+//     return x + y;
+// }
+
+
+// function plus(x, y) {
+//     return x + y;
+// }
+
+// plus(5, 8)
+// // plus(30, 50)
+
+// function check(a) {
+//     if (a > 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }                                                                                                        && --- va
+//                                                                                                          || --- yoki
+// check(-5)                                                                                                != --- inkor                    
+// function checkAB(a, b) {
+//     if (a > 0 || b > 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// checkAB(45 ,-5)
+
+
+// function age(birthYear) {
+//     return 2023 - birthYear
+// }
+
+// age(2005)
+
+// let num = 3.14;
+
+// let res =Math.ceil(Math.random() * 9)
+// // let res = Math.random(num)
+// // let res = Math.random(num)
+// // let res = Math.round(num)
+// // let res = Math.floor(num)
+// // let res = Math.ceil(num)0
+
+// console.log(res);
+
+// let followers = ['aziz', 'ali', 'vali', 'gani', 'qani']
+
+// let res =followers[Math.ceil(Math.random() * followers.length)]
+
+// console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+    // var num = -54;
+
+// if (num > 0) {
+//     console.log('musbat');
+// } else {
+//     console.log('manfiy');
+// }
+
+// var num = 4;
+
+// if (num % 2 == 0) {
+//     console.log('juft son');
+// } else {
+//     console.log('toq son');
+// }
+
+// var b = 5;
+// var a = 5;
+
+// if (b > 0 && a > 0) {
+//     console.log('ikkisi ham musbat');
+// } else {
+//     console.log('ikkisi ham manfiy yoki  bittasi manfiy');
+// }
+//  var studentScore = 90.7;
+
+
+//  if (studentScore >= 90) {
+//     console.log('You got A');
+//  } else if(studentScore < 90 && studentScore >= 80) {
+//     console.log('You got B');
+//  } else if (studentScore < 80 && studentScore >=70) {
+//     console.log('You got  C');
+//  } else if (studentScore < 70 && studentScore >= 60) {
+//     console.log('You got D');
+//  }  else {
+//     console.log('You got F,loser');
+//  }
+
+// var a = 22;
+// var b = 44;
+//  if (a % 2 ==0 && b % 2 ==0) {
+//     console.log('juft');
+//  } else {
+//     console.log('toq');
+//  }
+
+// var a = 3;
+// var b = 4;
+// var c = -12;
+// var d = 36;
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// var a = -25;
+// var b = 2;
+// var c = -14;
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log('barchasi musbat');
+// } else if(a > 0 && b < 0 && c > 0) {
+//     console.log('b manfiy');
+// } else if(a > 0 && b > 0 && c < 0) {
+//     console.log('c manfiy');
+// } else {
+//     console.log('a manfiy');
+// }
+
+// var week = 3;
+
+// if (week = 1) {
+//     console.log('Monday');
+// } else if(week = 2) {
+//     console.log('Tuesday');
+// } else if(week = 3) {
+//     console.log('Wednesday');
+// } else if(week = 4) {
+//     console.log('Thursday');
+// } else if(week = 5) {
+//     console.log('Friday');
+// } else if(week = 6) {
+//     console.log('Saturday');
+// } else{
+//     console.log('Sunday');
+// }
+
+
+// var code = 94;
+
+// if (code == 99 || code == 95) {
+//     console.log('UzMobile');
+// } else if(code == 93 || code == 94) {
+//     console.log('Ucell');
+// } else if(code == 90 || code == 91) {
+//     console.log('Beeline');
+// } else if(code == 97 || code == 77) {
+//     console.log('MobiUz');
+// } else {
+//     console.log('bunday kod bazada mavjud emas');
+// }
+
+// var num = -5;
+
+// if (num > 0) {
+//     console.log(++num);
+// } else {
+//     console.log(++num);
+// }    if else ichiga har doim ham string(text yozilavermaydi biror bir amal ham oshirishimz mumkin agar)
+
+
+// var num = -4;
+
+// if (num > 0) {
+//     console.log(num + 3);
+// } else {
+//     console.log(num + 2);
+// }
+
+// var a = 5;
+// var b = 25;
+
+// if (a > b) {
+//     console.log('a qiymati b qiymatidan katta');
+// } else {
+//     console.log('b qiymati a qiymatidan katta');
+// }
+
+// var a = 25;
+// var b = 104;
+// var c = 5;
+
+// if (a > b && a > c) {
+//     console.log('a qiymati b va c qiymatidan katta');
+// } else if(b > a && b > c) {
+//     console.log('b qiymati a va c qiymatidan katta');
+// } else {
+//     console.log('c qiymati a va b qiymatidan katta');
+// }
+
+// var a = 25;
+// var b = 4;
+// var c = 55;
+
+// if (a < b && a < c) {
+//     console.log('a qiymati b va c qiymatidan kichik');
+// } else if(b < a && b < c) {
+//     console.log('b qiymati a va c qiymatidan kichik');
+// } else {
+//     console.log('c qiymati a va b qiymatidan kichik');
+// }
+
+
+// var gender = 'female';
+
+// if (gender == 'male') {
+//     console.log('Erkak');
+// } else if(gender == 'female') {
+//     console.log('Ayol');
+// } else {
+//     console.log('Notogri jins kirittingiz');
+// }   Agar qiymatimiz oddiy text bo'ladigan bo'lsa va shartimizdagi solishtiradigan qiymatimz ham text bo'ladigan bo'lsa string xolatida yozishimz kerak!
+
+// var studentBall = 8;
+// if (studentBall >= 5) {
+//     console.log('Tabriklayman siz 1mln som stipendiyani qolga kirittingiz');
+// } else if(studentBall < 5 && studentBall >= 4) {
+//     console.log('Tabriklayman siz 500ming som stipendiyani qolga kirittingiz');
+// } else if(studentBall < 4 && studentBall >= 3) {
+//     console.log('Tabriklayman siz 300ming som stipendiyani qolga kirittingiz');
+// } else if(studentBall < 3 && studentBall >= 2) {
+//     console.log('Tabriklayman siz 100ming som stipendiyani qolga kirittingiz');
+// } else {
+//     console.log('Toshingni ter');
+// }
+
+// var num = -11;
+
+// if (num > 0 && num % 2 == 0) {
+//     console.log('Bu son juft musbat');
+// } else if(num < 0 && num % 2 == 0) {
+//     console.log('Bu son juft manfiy');
+// } else if(num > 0 && num % 2== 0){
+//     console.log('Bu son toq manfiy');
+// } else {
+//     console.log('Bu son toq musbat');
+// }
+
+// var num = -12;
+
+// if (num > 0 && num % 2 == 0) {
+//     console.log('Bu son juft musbat');
+// } else if(num < 0 && num % 2 == 0) {
+//     console.log('Bu son juft manfiy');
+// } else if(num > 0 && num % 2 != 0){
+//     console.log('Bu son toq musbat');
+// } else {
+//     console.log('Bu son toq manfiy');
+// }  ! inkor begisi bo'lgisi
+
+// var birthYears = 2004;
+// var nowYear = 2023;
+// var age = nowYear - birthYears;
+// console.log(age);
+
+// var a = 32;
+// var b = 12;
+// var c = 110;
+
+// if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+//     console.log('Barcha qiymatlar juft');
+// } else if(a % 2 == 0 && b % 2 != 0 && c % 2 == 0) {
+//     console.log('B qiymati toq');
+// } else if(a % 2 == 0 && b % 2 == 0 && c % 2 != 0) {
+//     console.log('C qiymati toq');
+// } else if(a % 2 != 0 && b % 2 != 0 && c % 2 == 0) {
+//     console.log('A va B qiymatlari toq');
+// } else if(a % 2 != 0 && b % 2 == 0 && c % 2 != 0) {
+//     console.log('A va C qiymatlari toq');
+// } else if(a % 2 == 0 && b % 2 != 0 && c % 2 != 0) {
+//     console.log('B va C qiymatlari toq');
+// } else if(a % 2 != 0 && b % 2 != 0 && c % 2 != 0) {
+//     console.log('Barcha qiymatlari toq');
+// }
+//  else {
+//     console.log('A qiymati toq');
+// }
+
+// var a = 10;
+// var b = 12;
+// var c = 45;
+// var num = [222,305,85]
+// let sorted = num.sort((a,b)=> a-b)
+// console.log(sorted);  
+}
---
+++ lesson3/main.js
@@ -0,0 +1,612 @@
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30
+// };
+// var car = {
+//     make: "Toyota",
+//     model: "Camry",
+//     year: 2022
+// };
+// var book = {
+//     title: "The Great Gatsby",
+//     author: "F. Scott Fitzgerald",
+//     pages: 192
+// };
+// var movie = {
+//     title: "Inception",
+//     director: "Christopher Nolan",
+//     year: 2010
+// };
+// var student = {
+//     name: "Alice",
+//     grade: "A",
+//     id: "12345"
+// };
+// var product = {
+//     name: "Laptop",
+//     price: 999.99,
+//     inStock: true
+// };
+// var color = {
+//     name: "Red",
+//     hexCode: "#FF0000"
+// };
+// var country = {
+//     name: "United States",
+//     population: 331000000,
+//     capital: "Washington, D.C."
+// };
+// var recipe = {
+//     name: "Spaghetti Bolognese",
+//     ingredients: ["spaghetti", "ground beef", "tomato sauce"]
+// };
+// var weather = {
+//     city: "New York",
+//     temperature: 75,
+//     conditions: "Sunny"
+// };
+// var person = {
+//     name: "Mary",
+//     age: 25,
+//     greet: function() {
+//         console.log("Hello, my name is " + this.name);
+//     }
+// };
+// var bankAccount = {
+//     accountNumber: "123456789",
+//     balance: 1000,
+//     deposit: function(amount) {
+//         this.balance += amount;
+//     },
+//     withdraw: function(amount) {
+//         this.balance -= amount;
+//     }
+// };
+// var bankAccount = {
+//     accountNumber: "123456789",
+//     balance: 1000,
+//     deposit: function(amount) {
+//         this.balance += amount;
+//     },
+//     withdraw: function(amount) {
+//         this.balance -= amount;
+//     }
+// };
+// var student = {
+//     name: "Bob",
+//     courses: {
+//         math: "A",
+//         history: "B",
+//         science: "A"
+//     }
+// };
+// var company = {
+//     name: "Acme Corporation",
+//     employees: 1000,
+//     isPublic: false
+// };
+// var song = {
+//     title: "Bohemian Rhapsody",
+//     artist: "Queen",
+//     duration: "6:07"
+// };
+// var phone = {
+//     brand: "iPhone",
+//     model: "12 Pro",
+//     price: 999.99
+// };
+// var city = {
+//     name: "Paris",
+//     population: 2141000,
+//     country: "France"
+// };
+// var animal = {
+//     species: "Tiger",
+//     habitat: "Jungle",
+//     isEndangered: true
+// };
+// var userProfile = {
+//     username: "johndoe",
+//     email: "john@example.com",
+//     isAdmin: false
+// };
+// var computer = {
+//     brand: "Dell",
+//     model: "XPS 13",
+//     specs: {
+//         cpu: "Intel i7",
+//         ram: "16GB",
+//         storage: "512GB SSD"
+//     }
+// };
+// var fruit = {
+//     name: "Apple",
+//     color: "Red",
+//     isSweet: true
+// };
+// var restaurant = {
+//     name: "Pizza Palace",
+//     cuisine: "Italian",
+//     rating: 4.5
+// };
+// var calendarEvent = {
+//     title: "Meeting",
+//     date: "2023-08-25",
+//     location: "Conference Room"
+// };
+// var ship = {
+//     name: "Titanic",
+//     type: "Passenger Ship",
+//     yearBuilt: 1912
+// };
+// var programmingLanguage = {
+//     name: "JavaScript",
+//     creator: "Brendan Eich",
+//     yearCreated: 1995
+// };
+// var countryFlag = {
+//     country: "Germany",
+//     flagColors: ["black", "red", "yellow"]
+// };
+// var email = {
+//     subject: "Meeting Agenda",
+//     sender: "alice@example.com",
+//     recipients: ["bob@example.com", "charlie@example.com"]
+// };
+// var libraryBook = {
+//     title: "To Kill a Mockingbird",
+//     author: "Harper Lee",
+//     isAvailable: true
+// };
+// var recipe = {
+//     name: "Chocolate Cake",
+//     ingredients: ["flour", "sugar", "cocoa"],
+//     steps: ["Mix dry ingredients", "Add eggs and milk", "Bake at 350°F"]
+// };
+// var movieTheater = {
+//     name: "CinemaPlex",
+//     location: "Downtown",
+//     screens: 5
+// };
+// var socialMediaPost = {
+//     text: "Having a great day!",
+//     likes: 50,
+//     comments: ["Nice!", "Glad to hear that!"]
+// };
+// var cityWeather = {
+//     city: "London",
+//     temperature: 65,
+//     conditions: "Partly Cloudy"
+// };
+// var employee = {
+//     name: "Jane",
+//     department: "HR",
+//     salary: 50000
+// };
+// var recipe = {
+//     name: "Vegetable Stir-Fry",
+//     ingredients: ["broccoli", "carrots", "tofu"],
+//     vegetarian: true,
+//     glutenFree: true
+// };
+// var currencyExchange = {
+//     fromCurrency: "USD",
+//     toCurrency: "EUR",
+//     rate: 0.85
+// };
+// var smartphone = {
+//     brand: "Samsung",
+//     model: "Galaxy S21",
+//     storageCapacity: "128GB"
+// };
+// var coffee = {
+//     name: "Espresso",
+//     origin: "Italy",
+//     caffeineContent: "High"
+// };
+// var customerOrder = {
+//     orderNumber: "12345",
+//     items: ["T-shirt", "Jeans", "Shoes"],
+//     totalAmount: 150
+// };
+// var socialProfile = {
+//     username: "user123",
+//     followers: 1000,
+//     following: 500
+// };
+// var airport = {
+//     code: "JFK",
+//     name: "John F. Kennedy International Airport",
+//     location: "New York"
+// };
+// var pet = {
+//     name: "Buddy",
+//     species: "Dog",
+//     age: 3
+// };
+// var laptop = {
+//     brand: "HP",
+//     model: "Pavilion",
+//     specs: {
+//         cpu: "Intel i5",
+//         ram: "8GB",
+//         storage: "256GB SSD"
+//     }
+// };
+// var videoGame = {
+//     title: "Minecraft",
+//     platform: "PC",
+//     genre: "Sandbox"
+// };
+// var camera = {
+//     brand: "Canon",
+//     model: "EOS Rebel T7i",
+//     megapixels: 24
+// };
+// var nationalPark = {
+//     name: "Yellowstone",
+//     location: "Wyoming, USA",
+//     size: "2.2 million acres"
+// };
+// var flight = {
+//     airline: "Delta",
+//     flightNumber: "DL123",
+//     departure: "2023-08-28 14:00",
+//     arrival: "2023-08-28 16:30"
+// };
+// var library = {
+//     name: "Main Library",
+//     location: "Downtown",
+//     books: [
+//         { title: "The Hobbit", author: "J.R.R. Tolkien" },
+//         { title: "1984", author: "George Orwell" },
+//         { title: "Pride and Prejudice", author: "Jane Austen" }
+//     ]
+// };
+
+
+
+// var num = -54;
+
+// if (num > 0) {
+//     console.log('musbat');
+// } else {
+//     console.log('manfiy');
+// }
+
+// var num = 4;
+
+// if (num % 2 == 0) {
+//     console.log('juft son');
+// } else {
+//     console.log('toq son');
+// }
+
+// var b = 5;
+// var a = 5;
+
+// if (b > 0 && a > 0) {
+//     console.log('ikkisi ham musbat');
+// } else {
+//     console.log('ikkisi ham manfiy yoki  bittasi manfiy');
+// }
+//  var studentScore = 90.7;
+
+
+//  if (studentScore >= 90) {
+//     console.log('You got A');
+//  } else if(studentScore < 90 && studentScore >= 80) {
+//     console.log('You got B');
+//  } else if (studentScore < 80 && studentScore >=70) {
+//     console.log('You got  C');
+//  } else if (studentScore < 70 && studentScore >= 60) {
+//     console.log('You got D');
+//  }  else {
+//     console.log('You got F,loser');
+//  }
+
+// var a = 22;
+// var b = 44;
+//  if (a % 2 ==0 && b % 2 ==0) {
+//     console.log('juft');
+//  } else {
+//     console.log('toq');
+//  }
+
+// var a = 3;
+// var b = 4;
+// var c = -12;
+// var d = 36;
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// var a = -25;
+// var b = 2;
+// var c = -14;
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log('barchasi musbat');
+// } else if(a > 0 && b < 0 && c > 0) {
+//     console.log('b manfiy');
+// } else if(a > 0 && b > 0 && c < 0) {
+//     console.log('c manfiy');
+// } else {
+//     console.log('a manfiy');
+// }
+
+// var week = 3;
+
+// if (week = 1) {
+//     console.log('Monday');
+// } else if(week = 2) {
+//     console.log('Tuesday');
+// } else if(week = 3) {
+//     console.log('Wednesday');
+// } else if(week = 4) {
+//     console.log('Thursday');
+// } else if(week = 5) {
+//     console.log('Friday');
+// } else if(week = 6) {
+//     console.log('Saturday');
+// } else{
+//     console.log('Sunday');
+// }
+
+
+// var code = 95;
+
+// if (code == 99 || code == 95) {
+//     console.log('UzMobile');
+// } else if(code == 93 || code == 94) {
+//     console.log('Ucell');
+// } else if(code == 90 || code == 91) {
+//     console.log('Beeline');
+// } else if(code == 97 || code == 77) {
+//     console.log('MobiUz');
+// } else {
+//     console.log('bunday kod bazada mavjud emas');
+// }
+
+// var num = -5;
+
+// if (num > 0) {
+//     console.log(++num);
+// } else {
+//     console.log(++num);
+// }    if else ichiga har doim ham string(text yozilavermaydi biror bir amal ham oshirishimz mumkin agar)
+
+
+// var num = -4;
+
+// if (num > 0) {
+//     console.log(num + 3);
+// } else {
+//     console.log(num + 2);
+// }
+
+// var a = 5;
+// var b = 25;
+
+// if (a > b) {
+//     console.log('a qiymati b qiymatidan katta');
+// } else {
+//     console.log('b qiymati a qiymatidan katta');
+// }
+
+// var a = 25;
+// var b = 104;
+// var c = 5;
+
+// if (a > b && a > c) {
+//     console.log('a qiymati b va c qiymatidan katta');
+// } else if(b > a && b > c) {
+//     console.log('b qiymati a va c qiymatidan katta');
+// } else {
+//     console.log('c qiymati a va b qiymatidan katta');
+// }
+
+// var a = 25;
+// var b = 4;
+// var c = 55;
+
+// if (a < b && a < c) {
+//     console.log('a qiymati b va c qiymatidan kichik');
+// } else if(b < a && b < c) {
+//     console.log('b qiymati a va c qiymatidan kichik');
+// } else {
+//     console.log('c qiymati a va b qiymatidan kichik');
+// }
+
+
+// var gender = 'female';
+
+// if (gender == 'male') {
+//     console.log('Erkak');
+// } else if(gender == 'female') {
+//     console.log('Ayol');
+// } else {
+//     console.log('Notogri jins kirittingiz');
+// }   Agar qiymatimiz oddiy text bo'ladigan bo'lsa va shartimizdagi solishtiradigan qiymatimz ham text bo'ladigan bo'lsa string xolatida yozishimz kerak!
+
+// var studentBall = 8;
+// if (studentBall >= 5) {
+//     console.log('Tabriklayman siz 1mln som stipendiyani qolga kirittingiz');
+// } else if(studentBall < 5 && studentBall >= 4) {
+//     console.log('Tabriklayman siz 500ming som stipendiyani qolga kirittingiz');
+// } else if(studentBall < 4 && studentBall >= 3) {
+//     console.log('Tabriklayman siz 300ming som stipendiyani qolga kirittingiz');
+// } else if(studentBall < 3 && studentBall >= 2) {
+//     console.log('Tabriklayman siz 100ming som stipendiyani qolga kirittingiz');
+// } else {
+//     console.log('Toshingni ter');
+// }
+
+// var num = -11;
+
+// if (num > 0 && num % 2 == 0) {
+//     console.log('Bu son juft musbat');
+// } else if(num < 0 && num % 2 == 0) {
+//     console.log('Bu son juft manfiy');
+// } else if(num > 0 && num % 2== 0){
+//     console.log('Bu son toq manfiy');
+// } else {
+//     console.log('Bu son toq musbat');
+// }
+
+// var num = -12;
+
+// if (num > 0 && num % 2 == 0) {
+//     console.log('Bu son juft musbat');
+// } else if(num < 0 && num % 2 == 0) {
+//     console.log('Bu son juft manfiy');
+// } else if(num > 0 && num % 2 != 0){
+//     console.log('Bu son toq musbat');
+// } else {
+//     console.log('Bu son toq manfiy');
+// }  ! inkor begisi bo'lgisi
+
+// var birthYears = 2004;
+// var nowYear = 2023;
+// var age = nowYear - birthYears;
+// console.log(age);
+
+// var a = 32;
+// var b = 12;
+// var c = 110;
+
+// if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+//     console.log('Barcha qiymatlar juft');
+// } else if(a % 2 == 0 && b % 2 != 0 && c % 2 == 0) {
+//     console.log('B qiymati toq');
+// } else if(a % 2 == 0 && b % 2 == 0 && c % 2 != 0) {
+//     console.log('C qiymati toq');
+// } else if(a % 2 != 0 && b % 2 != 0 && c % 2 == 0) {
+//     console.log('A va B qiymatlari toq');
+// } else if(a % 2 != 0 && b % 2 == 0 && c % 2 != 0) {
+//     console.log('A va C qiymatlari toq');
+// } else if(a % 2 == 0 && b % 2 != 0 && c % 2 != 0) {
+//     console.log('B va C qiymatlari toq');
+// } else if(a % 2 != 0 && b % 2 != 0 && c % 2 != 0) {
+//     console.log('Barcha qiymatlari toq');
+// }
+//  else {
+//     console.log('A qiymati toq');
+// }
+
+// var a = 10;
+// var b = 12;
+// var c = 45;
+// var num = [222,305,85]
+// let sorted = num.sort((a,b)=> a-b)
+// console.log(sorted);  agar tartib bo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const sum = 98 + "hello"
+
+// console.log(sum)
+
+// console.log(typeof sum);
+
+// var name = 1997 + "Nodir"
+
+// console.log(name)
+
+// console.log(typeof name);
+
+// let string = ""
+// let number = 18
+// let boolean = true
+
+// console.log(!number)
+
+// console.log(boolean + string)
+// console.log(18 + true)
+
+// let variable1 = 20
+// let variable2 = "20"
+
+// console.log(variable1 == variable2)
+
+
+// var sum1 = true
+// var sum2 = ""
+
+// console.log(sum1 == sum2)
+
+
+
+// const variable1 = 20
+// const variable2 = "20"
+
+// console.log(variable1 === variable2)
+
+// const variable3 = false
+// const variable4 = ""
+
+// console.log(variable3 === variable4)
+
+// const variable3 = false
+// const variable4 = ""
+
+// console.log(variable3 === variable4)
+
+// console.log(typeof numberConvert)
+
+
+// const number = 30
+
+// const numberConvert = Boolean(number)
+
+// console.log(numberConvert)
+
+// console.log(typeof numberConvert)
+
+
+// const boolean = false
+
+// const booleanConvert = String(boolean)
+
+// console.log(booleanConvert)
+
+// console.log(typeof booleanConvert)
+
+
+// const apiData = {
+//     rate: "50"
+//   }
+  
+//   console.log(apiData.rate === 50)
+
+// const apiData = {
+//     rate: "50"
+//   }
+  
+//   const rate = Number(apiData.rate)
+  
+//   console.log(rate === 50)
